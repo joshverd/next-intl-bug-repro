466ea64f5d05e44309f24f726df0710923dbb706
Joshua Verdehem <joshverd2@gmail.com>
2025-03-11 16:52:05 -0500

    Added comments

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,18 +3,22 @@ import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {};
  
-/**
- * This works for Webpack
- */
-const withNextIntl = createNextIntlPlugin(
-  'i18n/request.ts',
-);
-
 // /**
-//  * This works for STARTING the Turbo dev server but NOT when the layout.tsx file is run
+//  * This works for Webpack
+//  * 
+//  * But NOT for starting the Turbo dev server
 //  */
 // const withNextIntl = createNextIntlPlugin(
-//   'src/i18n/request.ts',
+//   'i18n/request.ts',
 // );
+
+/**
+ * This works for STARTING the Turbo dev server
+ * 
+ * But this will throw an error in layout.tsx when loading localhost:3000
+ */
+const withNextIntl = createNextIntlPlugin(
+  'src/i18n/request.ts',
+);
 
 export default withNextIntl(nextConfig);
